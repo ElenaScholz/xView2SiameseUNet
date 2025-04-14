175b563df505f40833cba6199c16a0ab8c631e90
ElenaScholz <elenascholz@hotmail.de>
2025-04-14 13:17:27 +0200

    updated preprocessing

--- notebooks/data_processing.py
+++ notebooks/data_processing.py
@@ -17,8 +17,30 @@ import numpy as np
 from utils.helper_functions import get_data_folders
 from utils.preprocessing import extract_features, load_label_data, process_label_metadata, process_features, make_label_dictionary, geotiff_converter, create_disaster_targets
 
+from pathlib import Path
+import matplotlib.pyplot as plt
+from PIL import Image
+from shapely.wkt import loads
+import json
+import os
+import pandas as pd
+import rasterio as rio
+from rasterio.features import rasterize
+import geopandas as gpd
+import numpy as np
+from shapely import wkt
+from shapely import Polygon
+import numpy as np
+
+
+from utils.helperfunctions import get_data_folder
+from utils.preprocessing import extract_features, load_label_data, process_label_metadata, process_features, make_label_dictionary, geotiff_converter, create_disaster_targets
+
 
 ROOT = Path("/dss/dsstbyfs02/pn49ci/pn49ci-dss-0022")
+DATA_PATH = ROOT / "data"
+
+
 ##########################################################
 # Define your username here:
 USER = "di97ren"
@@ -26,18 +48,7 @@ USER_PATH = ROOT / f"users/{USER}"
 
 
 
-labels = os.listdir(test_folder['labels'])
-print(labels)
-label_paths = []
-
-for l in labels:
-    label_paths.append(os.path.join(labels / l))
-
-label_data = []
-
-for label in label_paths:
-    with open(label, "r") as file:
-        label_data.append(pd.read_json(file))
+folder_to_prepocess = ["test", "tier1", "tier3", "hold"]
 
 # define building damage codes
 damage_codes = {
@@ -47,7 +58,29 @@ damage_codes = {
     'destroyed' : 4,
     'un-classified' : 5
 }
+for folder in folder_to_prepocess:
 
-label_dictionary = make_label_dictionary(test_folder['labels'], damage_codes)
 
-geotiff_converter(test_folder['images'], test_folder['png_images'])
+    DATASET_ROOT, DATA_FOLDER, IMAGE_FOLDER, LABEL_FOLDER, TARGET_FOLDER, PNG_FOLDER = get_data_folder(folder, main_dataset = True)
+
+    labels = os.listdir(LABEL_FOLDER)
+
+    label_paths = []
+
+    for l in labels:
+        label_paths.append(os.path.join(LABEL_FOLDER / l))
+
+    label_data = []
+
+    for label in label_paths:
+        with open(label, "r") as file:
+            label_data.append(pd.read_json(file))
+
+            
+    label_dictionary = make_label_dictionary(LABEL_FOLDER, damage_codes)
+
+    geotiff_converter(IMAGE_FOLDER, PNG_FOLDER)
+
+    create_disaster_targets(PNG_FOLDER, label_dictionary, TARGET_FOLDER)
+
+
---
+++ notebooks/utils/helperfunctions.py
@@ -0,0 +1,33 @@
+import os
+
+def iterate_through_dir(directory_path):
+    for directory_path, directorynames, filenames in os.walk(directory_path):
+        print(f"There are {len(directorynames)} directories and {len(filenames)} images in '{directory_path}'.")
+
+from pathlib import Path
+
+def get_data_folder(folder_name: str,
+    main_dataset: bool):  # possible names: ["test", "tier1", "tier3", "hold"] 
+    ROOT = Path("/dss/dsstbyfs02/pn49ci/pn49ci-dss-0022")
+
+    DATA_PATH = ROOT / "data"
+    
+
+    if main_dataset:
+        DATASET_ROOT = DATA_PATH / "xview2"
+        DATA_FOLDER = DATASET_ROOT / "test" # this has to be changed in respect to the folder (tier1, tier3, hold, test)
+        IMAGE_FOLDER = DATA_FOLDER / "images/"
+        LABEL_FOLDER = DATA_FOLDER / "labels/"
+        TARGET_FOLDER = DATA_FOLDER / "targets/"
+        PNG_FOLDER = DATA_FOLDER / "png_images/"
+
+    else:         # Path Configuration to the xview2 Subset
+        DATASET_ROOT = DATA_PATH / "xview2"
+        DATA_FOLDER = DATASET_ROOT / "test" # this has to be changed in respect to the folder (tier1, tier3, hold, test)
+        IMAGE_FOLDER = DATA_FOLDER / "images/"
+        LABEL_FOLDER = DATA_FOLDER / "labels/"
+        TARGET_FOLDER = DATA_FOLDER / "targets/"
+        PNG_FOLDER = DATA_FOLDER / "png_images/"
+
+    return DATASET_ROOT, DATA_FOLDER, IMAGE_FOLDER, LABEL_FOLDER, TARGET_FOLDER, PNG_FOLDER
+
