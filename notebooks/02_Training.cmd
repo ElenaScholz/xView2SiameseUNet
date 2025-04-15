a3c2c709947ea26f9ede01c38b92a4aa8e5059f3
ElenaScholz <elenascholz@hotmail.de>
2025-04-15 13:27:37 +0200

    changes in cmd file

--- notebooks/02_Training.cmd
+++ notebooks/02_Training.cmd
@@ -2,26 +2,32 @@
 #SBATCH -J xView2_model_Training
 #SBATCH -o /dss/dsstbyfs02/pn49ci/pn49ci-dss-0022/users/di97ren/xView2_Subset/logfiles/001/stdout.logfile
 #SBATCH -e /dss/dsstbyfs02/pn49ci/pn49ci-dss-0022/users/di97ren/xView2_Subset/logfiles/001/stderr.logfile
-#SBATCH -D /dss/dsstbyfs02/pn49ci/pn49ci-dss-0022/users/di97ren
+#SBATCH -D /dss/dsshome1/08/di97ren/04-geo-oma24/xView2SiameseUNet
 #SBATCH --clusters=hpda2
 #SBATCH --partition=hpda2_compute_gpu
-#SBATCH --gres=gpu:3  # Request 1 GPU
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=40    
 #SBATCH --mem=100gb
-#SBATCH --mail-type=all
+#SBATCH --mail-type=ALL
 #SBATCH --mail-user=elena.scholz@stud-mail.uni-wuerzburg.de
 #SBATCH --export=NONE
 #SBATCH --time=40:00:00
 #SBATCH --account=di97ren
 
-# Load necessary modules (if required by the cluster)
+# Lade Module
 module load slurm_setup
 module load python
 module load uv
 module load cuda
 
-# Python script
-script="02-DOTA_FasterRCNN.py"
+# Variablen
+VENV_PATH="/dss/dsshome1/08/di97ren/04-geo-oma24/xView2SiameseUNet/.venv"
+SCRIPT_PATH="/dss/dsshome1/08/di97ren/04-geo-oma24/xView2SiameseUNet/developer_main.py"
 
-# Run your Python script
-uv run --no-project -p /dss/dsshome1/0A/di38tac/04-geo-oma24/course_material_04_geo_oma24/.venv python /dss/dsshome1/0A/di38tac/DOTA-Net/code/$script
+# Debug-Ausgabe
+echo "Starte Training mit uv..."
+echo "Verwende virtuelle Umgebung: $VENV_PATH"
+echo "Führe Skript aus: $SCRIPT_PATH"
+
+# Starte Trainingsskript über uv
+uv run --no-project -p "$VENV_PATH" python "$SCRIPT_PATH"
