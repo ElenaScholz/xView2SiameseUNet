d4017fc790970aa3e8eea62d37a6c8ee6464c226
ElenaScholz <148858258+ElenaScholz@users.noreply.github.com>
2025-04-17 10:16:57 +0200

    Delete utils directory

--- utils/helper.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def iterate_through_dir(directory_path):
-    for directory_path, directorynames, filenames in os.walk(directory_path):
-        print(f"There are {len(directorynames)} directories and {len(filenames)} images in '{directory_path}'.")
